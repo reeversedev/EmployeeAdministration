b775082108e855e50e0e4c8d8899b1e39f95efa4
reeversedev <gogia.prateek@hotmail.com>
2017-03-31 04:20:43 +0530

    Add project files.

---
+++ EmployeeAdministration/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeAdministration.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+    }
+}
---
+++ EmployeeAdministration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeeAdministration.Startup))]
+namespace EmployeeAdministration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
